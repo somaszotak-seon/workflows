e4cc7e94fe1d759aaa09d0faf5bc45bfd7cf5333
Soma Szotak <soma.szotak@seon.io>
2025-07-15 14:58:42 +0200

    tingus

--- scripts/run-db-migrations.sh
+++ scripts/run-db-migrations.sh
@@ -9,15 +9,17 @@ if [ "$MODE" != "plan" ] && [ "$MODE" != "apply" ]; then
 fi
 
 if [ "$MODE" = "plan" ]; then
+  echo "SELECT * FROM seon_idv.sessions sx WHERE sx.id = '123';" > liquibase/liquibase-output
   COMMAND=update-sql
-  OPTIONS="--output-file=lb-diff.sql"
 elif [ "$MODE" = "apply" ]; then
+  echo "Update successful!" > liquibase/liquibase-output
+  echo "Executed: 1" >> liquibase/liquibase-output
+  echo "Failed: 0" >> liquibase/liquibase-output
+  echo "" >> liquibase/liquibase-output
+  echo "Started at: 2025-07-15 14:57:49" >> liquibase/liquibase-output
+  echo "Completed at: 2025-07-15 14:57:57" >> liquibase/liquibase-output
   COMMAND=update
-  OPTIONS=""
 fi
 
 cat .liquibase.env
-echo ""
-echo "docker compose -f docker-compose-lb.yml run --rm id-verification-liquibase liquibase $COMMAND $OPTIONS"
-ls -la
-echo "SELECT * FROM seon_idv.sessions sx WHERE sx.id = '123';" > ./liquibase/lb-diff.sql
+echo "docker compose -f docker-compose-lb.yml run --rm id-verification-liquibase liquibase $COMMAND --output-file=liquibase-output"
